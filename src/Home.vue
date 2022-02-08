fb64a654c577158ba07e66e751519ad131b41d9c
Rizcho <adde.asryadi@gmail.com>
2022-02-08 18:40:15 +0700

    1st commit

---
+++ src/Home.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+
+        <!-- navigation -->
+            <div class="flex sticky top-0 px-10 py-6 justify-between">
+                <div>kiri</div>
+                <div>
+                    <button class=" bg-green-300 px-3 py-2 rounded-2xl hover:bg-slate-600">TOMBOL</button>
+                </div>
+            </div>
+
+        <!-- content 1 -->
+            <div class="h-screen bg-slate-400">
+
+            </div>
+
+        <!-- content 2 -->
+            <div class="h-screen">
+                <div class="py-7">
+                    <p>Our brands</p>
+                </div>
+                <div>
+                     <div class="flex w-full h-auto border-t border-b border-blue-300 ">
+                        <div class="content-center justify-center w-1/4 border-r hover:bg-slate-500/70 translate-x-12 ease-in-out">
+                            <div class="pt-10">
+                                <div class="w-96 mx-auto"></div>
+                                <div class="">
+                                <img src="/public/img/img1.jpg" class="rounded-md w-96 mx-auto transition ease-in-out delay-150 hover:scale-110 duration-700" alt="">
+                                </div>
+                                
+                                <div class="flex justify-between px-10 pt-5 pb-9">
+                                    <div>KIRI</div>
+                                    <div>KANAN</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './Home.vue'
+import './index.css'
+
+createApp(App).mount('#app')
