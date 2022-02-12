979ac9b248e781709a17778cf70bfda6fd11eeb4
Rizcho <adde.asryadi@gmail.com>
2022-02-13 00:21:18 +0700

    content 4

--- src/Home.vue
+++ src/Home.vue
@@ -2,7 +2,7 @@
     <div class="text-white bg-mel1">
 
         <!-- navigation -->
-            <div class="sticky top-0 flex justify-between px-8 pt-5 bg-transparent ">
+            <div class="sticky top-0 flex justify-between px-8 pt-5 ">
                 <div class="flex text-4xl">
                     <p>melriver</p>
                     <p class="font-normal text-slate-600">/studio</p>
@@ -20,7 +20,7 @@
                     </button>
                 </div>
             </div>
-
+            
         <!-- content 1 -->
             <div class="relative h-screen">              
                 
@@ -76,49 +76,80 @@
             </div>
 
         <!-- content 3 -->
-        <div class="relative h-350  ">
-            
-            <div>
-                <div class="text-8xl pt-32 font-semibold px-44">
-                    <p>We're truly</p>    
-                    <p>sustainable.</p>    
-                </div>
-                <div class=" absolute pt-20 right-40  space-y-1 text-xl">
-                    <p>Sustainability is part of Melriver’s DNA since inception. Our ambition is</p>
-                    <p>to safeguard successful, long-term, sustainable brands in balance</p>
-                    <p>between social, environmental, and financial interests.</p>    
-                    <p class="pt-5">We are committed to creating sustainable products that are</p>
-                    <p>authentic, timeless and enduring. We design our products around</p>
-                    <p>circular principles and partner with our suppliers to source</p>
-                    <p>sustainable materials without compromising quality.</p>    
-                </div>
-                <div class="absolute flex bottom-10 right-44 space-x-52">
+            <div class="relative h-350">
+                <div>
+                    <div class="text-8xl pt-32 font-semibold px-44">
+                        <p>We're truly</p>    
+                        <p>sustainable.</p>    
+                    </div>
+                    <div class=" absolute pt-20 right-40  space-y-1 text-xl">
+                        <p>Sustainability is part of Melriver’s DNA since inception. Our ambition is</p>
+                        <p>to safeguard successful, long-term, sustainable brands in balance</p>
+                        <p>between social, environmental, and financial interests.</p>    
+                        <p class="pt-5">We are committed to creating sustainable products that are</p>
+                        <p>authentic, timeless and enduring. We design our products around</p>
+                        <p>circular principles and partner with our suppliers to source</p>
+                        <p>sustainable materials without compromising quality.</p>    
+                    </div>
+                    <div class="absolute flex bottom-10 right-44 space-x-52">
                         <div>
-                        <div class="py-8 border-t w-100 border-slate-600" v-for="data in datas" :key="data">
-                            <div class="pt-2 flex justify-between">
-                                <p class="text-7xl "> {{data.number}} </p>
-                                <p> {{data.unit}} </p>     
+                            <div class="py-8 border-t w-100 border-slate-600" v-for="data in datas" :key="data">
+                                <div class="pt-2 flex justify-between">
+                                    <p class="text-7xl "> {{data.number}} </p>
+                                    <p> {{data.unit}} </p>     
+                                </div>
+                                <div>
+                                    <p class="pt-8 text-xl text-slate-500/70"> {{data.txt}} </p>
+                                </div>
                             </div>
-                            <div>
-                                <p class="pt-8 text-xl text-slate-500/70"> {{data.txt}} </p>
-                            </div>
-                        </div>
                         </div>
                         <div>
-                        <div class="py-8 border-t w-100 border-slate-600" v-for="data in datas" :key="data">
-                            <div class="pt-2 flex justify-between">
-                                <p class="text-7xl "> {{data.number}} </p>
-                                <p> {{data.unit}} </p>     
-                            </div>
-                            <div>
-                                <p class="pt-8 text-xl text-slate-500/70"> {{data.txt}} </p>
+                            <div class="py-8 border-t w-100 border-slate-600" v-for="data in datas" :key="data">
+                                <div class="pt-2 flex justify-between">
+                                    <p class="text-7xl "> {{data.number}} </p>
+                                    <p> {{data.unit}} </p>     
+                                </div>
+                                <div>
+                                    <p class="pt-8 text-xl text-slate-500/70"> {{data.txt}} </p>
+                                </div>
                             </div>
                         </div>
-                        </div>
+                    </div>     
                 </div>
-                    
             </div>
-        </div>
+
+        <!-- content 4 -->
+            <div class="h-450 relative bg-white text-mel1">
+                <div>
+                    <div class="text-8xl pt-32 font-semibold px-44">
+                        <p>We aim for</p>    
+                        <p>timeless.</p>    
+                    </div>
+                    <div class="absolute pt-20 right-40 space-y-1 text-xl">
+                        <p>One reason why people want to use certain products and services is</p>
+                        <p>they like it, it is aesthetically pleasing. Timeless design includes the</p>
+                        <p>aesthetic quality and high standard of a product and it is integral to its</p>
+                        <p>usefulness due to its frequent usage. An ageless designed product</p>
+                        <p>will have a profound effect on people and their wellbeing, it can even</p>
+                        <p>change their habits and behavior.</p>    
+                    </div>
+                    
+                </div>
+                <div class="bg-slate-400 absolute inset-x-0 bottom-32 mx-auto w-4/5 h-2/4 ">
+                        <div class="pt-5 pl-6">
+                            <p class="text-white font-semibold">1/4</p>
+                        </div>
+                        <img src="" alt="">
+                        <div class="absolute right-3 bottom-3">
+                            <button class="text-4xl flex text-center pb-1 font-extralight text-white px-8 bg-gray-500/50 rounded-full">
+                                +
+                            </button>
+                        </div>
+                    </div>
+            </div>
+
+            
+
 
 
     </div>
